17156c522d9d09ca89a2a272f7ba9153a5d25cad
Peter Barker <pbarker@barker.dropbear.id.au>
2022-04-15 18:38:34 +1000

    AP_NavEKF3: move beacon variables into structure

--- libraries/AP_NavEKF3/AP_NavEKF3_core.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_core.cpp
@@ -344,45 +344,8 @@ void NavEKF3_core::InitialiseVariables()
 
     // range beacon fusion variables
 #if EK3_FEATURE_BEACON_FUSION
-    memset((void *)&rngBcnDataDelayed, 0, sizeof(rngBcnDataDelayed));
-    lastRngBcnPassTime_ms = 0;
-    rngBcnTestRatio = 0.0f;
-    rngBcnHealth = false;
-    varInnovRngBcn = 0.0f;
-    innovRngBcn = 0.0f;
-    memset(&lastTimeRngBcn_ms, 0, sizeof(lastTimeRngBcn_ms));
-    rngBcnDataToFuse = false;
-    beaconVehiclePosNED.zero();
-    beaconVehiclePosErr = 1.0f;
-    rngBcnLast3DmeasTime_ms = 0;
-    rngBcnGoodToAlign = false;
-    lastRngBcnChecked = 0;
-    receiverPos.zero();
-    memset(&receiverPosCov, 0, sizeof(receiverPosCov));
-    rngBcnAlignmentStarted =  false;
-    rngBcnAlignmentCompleted = false;
-    lastBeaconIndex = 0;
-    rngBcnPosSum.zero();
-    numBcnMeas = 0;
-    rngSum = 0.0f;
-    N_beacons = 0;
-    maxBcnPosD = 0.0f;
-    minBcnPosD = 0.0f;
-    bcnPosDownOffsetMax = 0.0f;
-    bcnPosOffsetMaxVar = 0.0f;
-    maxOffsetStateChangeFilt = 0.0f;
-    bcnPosDownOffsetMin = 0.0f;
-    bcnPosOffsetMinVar = 0.0f;
-    minOffsetStateChangeFilt = 0.0f;
-    rngBcnFuseDataReportIndex = 0;
-    if (dal.beacon()) {
-        if (rngBcnFusionReport == nullptr) {
-            rngBcnFusionReport = new rngBcnFusionReport_t[dal.beacon()->count()];
-        }
-    }
-    bcnPosOffsetNED.zero();
-    bcnOriginEstInit = false;
-#endif  // EK3_FEATURE_BEACON_FUSION
+    bcn.InitialiseVariables();
+#endif
 
 #if EK3_FEATURE_BODY_ODOM
     // body frame displacement fusion
@@ -450,6 +413,17 @@ void NavEKF3_core::InitialiseVariables()
     effectiveMagCal = effective_magCal();
 }
 
+void AP_NavEKF_core::BeaconFusion::InitialiseVariables()
+{
+    memset(this, 0, sizeof(*this));
+    VehiclePosErr = 1.0f;
+
+    if (dal.beacon()) {
+        if (rngBcnFusionReport == nullptr) {
+            rngBcnFusionReport = new rngBcnFusionReport_t[dal.beacon()->count()];
+        }
+    }
+}
 
 // Use a function call rather than a constructor to initialise variables because it enables the filter to be re-started in flight if necessary.
 void NavEKF3_core::InitialiseVariablesMag()
