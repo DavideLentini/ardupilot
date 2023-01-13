f8221498bc2d7ebe98d35ed5e653189e42c63fb3
Peter Barker <pbarker@barker.dropbear.id.au>
2023-01-13 11:27:10 +1100

    autotest: add test for HobbyWing Platinum Pro v3
    not much to test for this driver - we only use RPM

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -4144,6 +4144,39 @@ class AutoTestPlane(AutoTest):
             0) # button mask
         self.fly_home_land_and_disarm()
 
+    def HobbyWing_Platinum_PRO_v3(self):
+        '''test support for v3 of the HobbyWing Platinum PRO protocol'''
+        self.context_push()
+
+        self.set_parameters({
+            "SERIAL5_PROTOCOL": 45,
+            "RPM1_TYPE": 5,
+            "RPM1_ESC_MASK": 4,
+            "SERVO_H_V3_MASK": 4,
+            "SIM_ESC_TELEM": 0,
+        })
+        self.customise_SITL_commandline([
+            "--uartF=sim:hobbywing_platinum_pro_v3:3",
+        ])
+
+        self.assert_received_message_field_values("RPM", {
+            "rpm1": 0,
+            "rpm2": -1,
+        })
+
+        self.wait_ready_to_arm()
+        self.arm_vehicle()
+        self.change_mode('TAKEOFF')
+
+        self.wait_message_field_values("RPM", {
+            "rpm1": 5000,
+        }, epsilon=100)
+
+        self.disarm_vehicle(force=True)
+
+        self.context_pop()
+        self.reboot_sitl()
+
     def tests(self):
         '''return list of all tests'''
         ret = super(AutoTestPlane, self).tests()
@@ -4224,6 +4257,7 @@ class AutoTestPlane(AutoTest):
             self.AerobaticsScripting,
             self.MANUAL_CONTROL,
             self.WindEstimates,
+            self.HobbyWing_Platinum_PRO_v3,
         ])
         return ret
 
